21ce2e09471baaeea491cfada18bef52808fb9fe
Milena Kari <milenakari@yahoo.com>
2019-08-21 19:36:43 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,2 +1,25 @@
 // use this to copy code snippets or use your browser's console
-console.log('test')
+class Event {
+  constructor (title, keywords) {
+    this.title = title;
+    this.keywords=keywords;
+  }
+}
+
+class User {
+  consructor(name, interests){
+    this.name = name;
+    this.interests = interests;
+  }
+  
+  matchInterests(event){
+    return event.keywords.some(function(word){
+      return this.interests.includes(word)
+    });
+  }
+}
+
+let billy = new User('billy', ['music', 'art', 'movies'])
+let freeMusic = new Event('Free MusicShow', ['music', 'free', 'outside'])
+
+console.log(billy.matchInterests(freeMusic))
